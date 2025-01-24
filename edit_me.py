5953be969c5437ee396ee42ecebd37662475f2c5
Jude <judebrooks2023@gmail.com>
2025-01-24 10:15:08 -0600

    beginning the lab
    man... I created some variables and test lists and made sure the function could read the lists

--- edit_me.py
+++ edit_me.py
@@ -1,11 +1,21 @@
-
+#Jude Brooks
+#Lab 2, Data Structures
 def monotonic(lst):
     """Return True if lst is monotonic; return False, otherwise."""
-    #
-    # YOUR IMPLEMENTATION GOES HERE
-    #
+    len_lst = len(lst)
+    intA = lst[0]
+    intB = lst[1]
+    intC = lst[2]
+    if intA - intB >= 0 and intA != intC:
+        return True
+    else:
+        return False
 
-#
-# Feel free to replace these comments with
-# code that tests your function monotonic.
-#
+
+lstA = [1, 1, 3, 100] #This is ascending, monotonic
+lstB = [11, 1, -9, -10] #This is descending, monotonic
+lstC = [2, 3, 2, 3] #This is neither, non-monotonic
+
+print(monotonic(lstA))
+print(monotonic(lstB))
+print(monotonic(lstC))
