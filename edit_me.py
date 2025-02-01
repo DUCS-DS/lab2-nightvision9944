6c43ead702962b99ac5878a12874b17e65557a9d
Jude <judebrooks2023@gmail.com>
2025-01-31 19:52:18 -0600

    removed bad code
    code was not good, as in it ran too many times :/

--- edit_me.py
+++ edit_me.py
@@ -12,23 +12,14 @@ def monotonic(lst):
         if j == False:
             return 'This list is not monotonic'
         else:
-            return 'This list is monotonic, ascending'
-    #for k in range(len(lst)-1):
-    #    if lst[k] >= lst[k+1]: #List B works with this one :)
-    #        print('h')
-    #    else:
-    #        print('i')
-    #for l in range(len(lst)-1):
-    #    if lst[l] == lst[l+1]:
-    #        print('h')
-    #    else:
-    #        print('i')
+            return 'This list is monotonic'
+
 
 
 lstA = [1, 1, 3, 100] #This is ascending, monotonic
 lstB = [11, 1, -9, -10] #This is descending, monotonic
 lstC = [2, 3, 2, 3] #This is neither, non-monotonic
 
-x = monotonic(lstA)
+x = monotonic(lstC)
 print(x)
 
